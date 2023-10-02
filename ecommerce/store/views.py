29576f18fba3b189be525dbda21ed264d6e35376
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-03 03:30:24 +0800

    Modified cart view to render total items and total price of Order model

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -14,9 +14,10 @@ def cart(request):
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
+        order = {'get_cart_total': 0, 'get_cart_items': 0,}
         items = []
 
-    context = {'items': items}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
