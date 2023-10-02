a34d8be1cfc387e06948853813c4ed861080771b
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-02 20:12:53 +0800

    Registered all models to admin site

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.register(Customer)
+admin.register(Product)
+admin.register(Order)
+admin.register(OrderItem)
+admin.register(ShippingAddress)
