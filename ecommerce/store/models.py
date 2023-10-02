aee2b71538632eb214db5e7ab1a3bf126710af40
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-02 15:05:57 +0800

    Created model for products

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -3,3 +3,19 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
