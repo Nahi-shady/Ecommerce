5f3eabab7a2b049624285124195e0f02419b7512
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-02 19:53:32 +0800

    Created model for Orders

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -15,7 +15,17 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=50, null=True)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=True, blank=True)
+    digital = models.BooleanField(default=False, null=True, blank=False)
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.Set_Null, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=50, null=True)
+    
+    def __str__(self):
+        return str(self.id)
