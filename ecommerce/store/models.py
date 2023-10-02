629a306ad33eee2724ca7ea267f061aa1066576b
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-02 20:07:47 +0800

    Debugged some typos and Applied migration for all models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -22,7 +22,7 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.Set_Null, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=50, null=True)
@@ -31,7 +31,7 @@ class Order(models.Model):
         return str(self.id)
 
 
-class OrderItem(models.Mode):
+class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(blank=True, null=True)
@@ -39,7 +39,7 @@ class OrderItem(models.Mode):
 
 
 class ShippingAddress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.Set_Null, null=True, blank=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     address = models.CharField(max_length=100, null=True)
     city = models.CharField(max_length=100, null=True)
