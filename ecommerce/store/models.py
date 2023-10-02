7f961e49e0ebd1d73973f1358894901664876e87
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-02 20:04:30 +0800

    Created model for Shipping address

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -36,3 +36,16 @@ class OrderItem(models.Mode):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.Set_Null, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
+    zipcode = models.CharField(max_length=5100, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+
