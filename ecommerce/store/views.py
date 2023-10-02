61bac792fe188f10e6bff5dd30e684eb79e129d5
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-03 02:28:02 +0800

    Debugged complete not set to bolean in cart view

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -11,7 +11,7 @@ def store(request):
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=complete)
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
     else:
         items = []
