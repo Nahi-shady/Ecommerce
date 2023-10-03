fb8f438705231fd54f9ea2faf4461fb737ca5d2f
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-03 16:56:35 +0800

    Created updateUserOrder javascrtipt function to fetch request to updateItem view

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -10,7 +10,28 @@ for(var i = 0; i < updateBtns.length; i++){
         if(user === 'AnonymousUser'){
             console.log('not logged in')
         }else{
-            console.log('User is logged in')
+            updateUserOrder(productId, action)
         }
+    })
+}
+
+function updateUserOrder(productId, action){
+    console.log('User logged in, sending data...')
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({'productId: ': productId, 'action: ': action})
+    })
+
+    .then((response) =>{
+        return response.json()
+    })
+
+    .then((data) =>{
+        console.log('data', data)
     })
 }
