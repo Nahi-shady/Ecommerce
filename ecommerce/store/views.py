c063ed0ce3ea54515b4aba787abdd8f452a53473
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-03 16:19:57 +0800

    Created updateItem view to handle add to cart function

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product, Order
+from django.http import JsonResponse
 # Create your views here.
 
 def store(request):
@@ -32,3 +33,6 @@ def checkout(request):
 
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('item was added', safe=False)
