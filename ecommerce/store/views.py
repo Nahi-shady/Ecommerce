d0c37033e2aa2a89b16b3452cda6afc3b6ea99bc
Nahom Zewdu <nahiyo86@gmail.com>
2023-10-10 01:50:10 +0800

    Added update cart functionality to unAuthenticated users by parsing cookies

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -32,10 +32,17 @@ def cart(request):
         cartItems = order.get_cart_items
 
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+
         order = {'get_cart_total': 0, 'get_cart_items': 0,}
         items = []
         cartItems = order['get_cart_items']
 
+        for i in cart:
+            cartItems += cart[i]['quantity']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
